1c820760ec50f75ac84e7c82e932c5fb87d6b316
evelyn <eli_home@outlook.com>
2017-03-29 23:38:55 +0800

    only decode

--- hello.py
+++ hello.py
@@ -61,7 +61,7 @@ def domainSearch():
     indexes = r.zrevrange('researchInterest:'+domain, 0, -1, withscores=True)
     authors = []
     for index in indexes:
-        authors.append(r.hget('author:'+index[0], 'name').decode('utf-8').encode('ascii'))
+        authors.append(r.hget('author:'+index[0], 'name').decode('utf-8'))
     return render_template('domainSearchResult.html', authors = authors)
 
 @app.route('/login')
